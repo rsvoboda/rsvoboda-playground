18e3ae3f8f3093d3c63560176e5ac25d2c2b8fed
Rostislav Svoboda <rsvoboda@redhat.com>
2017-08-25 16:09:29 +0200

    Initiate Redux, counter increment non-redux way

--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgRedux, NgReduxModule} from 'ng2-redux';
+
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer } from './store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, {})
+  }
+ }
