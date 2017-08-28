b7c5a5e01961deb84667206ddff0b820c3394263
Rostislav Svoboda <rsvoboda@redhat.com>
2017-08-28 13:26:18 +0200

    Redux - DevToolsExtension

--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {NgRedux, NgReduxModule} from 'ng2-redux';
+import {NgRedux, NgReduxModule, DevToolsExtension} from 'ng2-redux';
 
 import { AppComponent } from './app.component';
 import { IAppState, INITIAL_STATE, rootReducer } from './store';
@@ -18,7 +18,12 @@ import { IAppState, INITIAL_STATE, rootReducer } from './store';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE)  // pass initial state of the store
+  constructor(ngRedux: NgRedux<IAppState>, private devTools: DevToolsExtension) {
+    let enhancers = [];
+    if (devTools.isEnabled()) {
+      enhancers = [ ...enhancers, devTools.enhancer() ];
+    }
+
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancers)  // pass initial state of the store
   }
  }
