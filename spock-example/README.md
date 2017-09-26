13ffc33e778309ccf16cc9c661225a10ffbfd451
Rostislav Svoboda <rsvoboda@redhat.com>
2017-09-26 16:44:04 +0200

    Spock data pipes

--- spock-example/README.md
+++ spock-example/README.md
@@ -5,6 +5,8 @@ Experiment project with testing framework Spock - http://spockframework.org/
 
 More examples: https://github.com/spockframework/spock-example/tree/master/src/test/groovy
 
+Documentation: http://spockframework.org/spock/docs/1.1/all_in_one.html
+
 Running
 -------------------
 ```bash
--- spock-example/src/test/groovy/MathOpsSpec.groovy
+++ spock-example/src/test/groovy/MathOpsSpec.groovy
@@ -2,14 +2,24 @@ import spock.lang.Specification;
 
 class MathOpsSpec extends Specification {
 
-  def "maximum of two numbers"(int a, int b, int c) {
-    expect:
-    Math.max(a, b) == c
+    def "maximum of two numbers"(int a, int b, int c) {
+        expect:
+        Math.max(a, b) == c
 
-    where:
-    a | b | c
-    1 | 3 | 3
-    7 | 4 | 7
-    0 | 0 | 0
-  }
+        where:
+        a | b | c
+        1 | 3 | 3
+        7 | 4 | 7
+        0 | 0 | 0
+    }
+
+    def "maximum of two numbers via pipes"(int a, int b, int c) {
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a << [1, 7, 0]
+        b << [3, 4, 0]
+        c << [3, 7, 0]
+    }
 }
