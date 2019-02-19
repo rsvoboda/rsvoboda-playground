4a54bfbd5097858d6be49c54884df11e21b9c1c2
Rostislav Svoboda <rsvoboda@redhat.com>
2019-02-19 20:24:21 +0100

    MicroProfile Starter based project

---
+++ mpdemo/src/main/java/org/jboss/test/rsvoboda/mpdemo/MpdemoRestApplication.java
@@ -0,0 +1,19 @@
+package org.jboss.test.rsvoboda.mpdemo;
+
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import javax.annotation.security.DeclareRoles;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+@LoginConfig(authMethod = "MP-JWT", realmName = "jwt-jaspi")
+@DeclareRoles({"protected"})
+
+public class MpdemoRestApplication extends Application {
+}
