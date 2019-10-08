718efa8e52078d693ae3b8d04a51836e3f8f0032
Rostislav Svoboda <rsvoboda@redhat.com>
2019-10-08 14:57:30 +0200

    Quarkus based project skeleton

---
+++ jfreesvg/jfreesvg-quarkus/src/test/java/cz/rsvoboda/NativePingResourceIT.java
@@ -0,0 +1,9 @@
+package cz.rsvoboda;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativePingResourceIT extends PingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
