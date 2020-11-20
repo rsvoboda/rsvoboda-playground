966b1eceb57350f0833b1597d0df6455e3b512c8
Rostislav Svoboda <rsvoboda@redhat.com>
2020-11-20 09:43:11 +0100

    Move jfreesvg-quarkus to Quarkus 1.10.0.Final

--- jfreesvg/jfreesvg-quarkus/src/test/java/cz/rsvoboda/NativePingResourceIT.java
+++ jfreesvg/jfreesvg-quarkus/src/test/java/cz/rsvoboda/NativePingResourceIT.java
@@ -1,8 +1,8 @@
 package cz.rsvoboda;
 
-import io.quarkus.test.junit.SubstrateTest;
+import io.quarkus.test.junit.NativeImageTest;
 
-@SubstrateTest
+@NativeImageTest
 public class NativePingResourceIT extends PingResourceTest {
 
     // Execute the same tests but in native mode.
